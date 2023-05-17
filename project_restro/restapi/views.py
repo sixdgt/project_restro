087a0d5898f7bf507825c6acb25fbdf988963820
sixdgt <c4crypt@gmail.com>
2023-05-17 18:03:14 +0545

    api documentation and standard structure for response

--- project_restro/restapi/views.py
+++ project_restro/restapi/views.py
@@ -7,22 +7,45 @@ from app_restro.models import Category, Menu
 from .serializers import CategorySerializer, MenuSerializer
 
 # Create your views here.
+class ApiResponse():
+    def success_response(self, message, code, data=dict()):
+        context = {
+            "message": message,
+            "status_code": code,
+            "data": data,
+            "error": []
+        }
+        return context
+
+    def error_response(self, message, code, error=dict()):
+        context = {
+            "message": message,
+            "status_code": code,
+            "data": [],
+            "error": error
+        }
+        return context
 class CategoryApiView(APIView):
+    response = ApiResponse()
+
     def get(self, request):
         categories = Category.objects.all()
         if not Category.DoesNotExist:
             return Response({"message": "Data not found"}, status=status.HTTP_404_NOT_FOUND)
         
         serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(self.response.success_response("Category List", 200, serializer.data), 
+                        status=status.HTTP_200_OK)
 
     def post(self, request):
         data = request.data
         serializer = CategorySerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(self.response.success_response("Success", 200, serializer.data),
+                             status=status.HTTP_201_CREATED)
+        return Response(self.response.error_response("Something went wrong", 400, serializer.errors), 
+                        status=status.HTTP_400_BAD_REQUEST)
 
 class CategoryApiIdView(APIView):
     def get_object(self, id):
